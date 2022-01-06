c5829213818de2b33a7964e6e784ed2ac81df60f
Whizzy Ellah <prisypauline@gmail.com>
2022-01-06 01:05:31 +0000

    First commit

---
+++ Error Provider Form Project/Form1.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.NameLabel = New System.Windows.Forms.Label()
+        Me.AgeLabel = New System.Windows.Forms.Label()
+        Me.NameTextBox = New System.Windows.Forms.TextBox()
+        Me.AgeTextBox = New System.Windows.Forms.TextBox()
+        Me.ButtonOkay = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
+        Me.ErrorProvider = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.ToolStrip1.SuspendLayout()
+        CType(Me.ErrorProvider, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'NameLabel
+        '
+        Me.NameLabel.AutoSize = True
+        Me.NameLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NameLabel.Location = New System.Drawing.Point(47, 104)
+        Me.NameLabel.Name = "NameLabel"
+        Me.NameLabel.Size = New System.Drawing.Size(69, 25)
+        Me.NameLabel.TabIndex = 0
+        Me.NameLabel.Text = " Name"
+        '
+        'AgeLabel
+        '
+        Me.AgeLabel.AutoSize = True
+        Me.AgeLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AgeLabel.Location = New System.Drawing.Point(47, 166)
+        Me.AgeLabel.Name = "AgeLabel"
+        Me.AgeLabel.Size = New System.Drawing.Size(53, 25)
+        Me.AgeLabel.TabIndex = 1
+        Me.AgeLabel.Text = " Age"
+        '
+        'NameTextBox
+        '
+        Me.NameTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NameTextBox.Location = New System.Drawing.Point(145, 108)
+        Me.NameTextBox.Name = "NameTextBox"
+        Me.NameTextBox.Size = New System.Drawing.Size(166, 30)
+        Me.NameTextBox.TabIndex = 2
+        '
+        'AgeTextBox
+        '
+        Me.AgeTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AgeTextBox.Location = New System.Drawing.Point(145, 166)
+        Me.AgeTextBox.Name = "AgeTextBox"
+        Me.AgeTextBox.Size = New System.Drawing.Size(87, 30)
+        Me.AgeTextBox.TabIndex = 3
+        '
+        'ButtonOkay
+        '
+        Me.ButtonOkay.BackColor = System.Drawing.Color.RoyalBlue
+        Me.ButtonOkay.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonOkay.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ButtonOkay.Location = New System.Drawing.Point(46, 309)
+        Me.ButtonOkay.Name = "ButtonOkay"
+        Me.ButtonOkay.Size = New System.Drawing.Size(125, 39)
+        Me.ButtonOkay.TabIndex = 4
+        Me.ButtonOkay.Text = "Ok"
+        Me.ButtonOkay.UseVisualStyleBackColor = False
+        '
+        'ExitButton
+        '
+        Me.ExitButton.BackColor = System.Drawing.Color.DarkRed
+        Me.ExitButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ExitButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ExitButton.Location = New System.Drawing.Point(201, 309)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(125, 39)
+        Me.ExitButton.TabIndex = 5
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = False
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 398)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(380, 22)
+        Me.StatusStrip1.TabIndex = 6
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripLabel1})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(380, 25)
+        Me.ToolStrip1.TabIndex = 7
+        Me.ToolStrip1.Text = "String.Empty"
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.BackColor = System.Drawing.Color.Salmon
+        Me.ToolStripLabel1.Font = New System.Drawing.Font("Segoe UI Semibold", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ToolStripLabel1.ForeColor = System.Drawing.Color.Tomato
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(0, 22)
+        '
+        'ErrorProvider
+        '
+        Me.ErrorProvider.ContainerControl = Me
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(380, 420)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ButtonOkay)
+        Me.Controls.Add(Me.AgeTextBox)
+        Me.Controls.Add(Me.NameTextBox)
+        Me.Controls.Add(Me.AgeLabel)
+        Me.Controls.Add(Me.NameLabel)
+        Me.Name = "Form1"
+        Me.Text = "ErrorProviderForm"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        CType(Me.ErrorProvider, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents NameLabel As System.Windows.Forms.Label
+    Friend WithEvents AgeLabel As System.Windows.Forms.Label
+    Friend WithEvents NameTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents AgeTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents ButtonOkay As System.Windows.Forms.Button
+    Friend WithEvents ExitButton As System.Windows.Forms.Button
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Private ReadOnly mNameMessage As String = "Name cannot be blank"
+    Private ReadOnly mAgeMessage As String = "Age must be a number"
+    Private ReadOnly mValidateMessage As String = "All user input is valid!"
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub ValidateName(ByVal ctrl As Control)
+        If NameTextBox.Text.Length = 0 Then
+            ErrorProvider.SetError(ctrl, mNameMessage)
+            NameTextBox.Focus()
+        Else
+            ErrorProvider.SetError(ctrl, String.Empty)
+        End If
+    End Sub
+    Private Sub ValidateAge(ByVal ctrl As Control)
+        If Not IsNumeric(AgeTextBox.Text) Then
+            ErrorProvider.SetError(ctrl, mAgeMessage)
+            AgeTextBox.Focus()
+        Else
+            ErrorProvider.SetError(ctrl, String.Empty)
+        End If
+    End Sub
+
+
+    Private Sub NameTextBox_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NameTextBox.Leave, NameTextBox.TextChanged
+        ValidateName(CType(sender, Control))
+
+    End Sub
+
+    Private Sub AgeTextBox_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NameTextBox.Leave, AgeTextBox.TextChanged
+        ValidateAge(CType(sender, Control))
+
+    End Sub
+    Friend WithEvents ErrorProvider As System.Windows.Forms.ErrorProvider
+
+    Private Sub SubmitButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonOkay.Click
+        ValidateAge(AgeTextBox)
+        With ErrorProvider
+            If .GetError(NameTextBox).Length <> 0 Then
+                ToolStripLabel1.Text = mNameMessage
+            ElseIf .GetError(AgeTextBox).Length <> 0 Then
+                ToolStripLabel1.Text = mAgeMessage
+            Else
+                ToolStripLabel1.Text = mValidateMessage
+            End If
+        End With
+
+    End Sub
+    Friend WithEvents ToolStripLabel1 As System.Windows.Forms.ToolStripLabel
+
+    Private Sub ExitButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+End Class
